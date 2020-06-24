bf1ee62082a596ad5a0fb5eee31f388bff22feda
Igor-Felipy <igor.felipy123@gmail.com>
2020-06-24 13:33:07 -0400

    consultar finalistas

--- db.py
+++ db.py
@@ -1,9 +1,8 @@
 import sqlite3
 
-conn = sqlite3.connect('data.db')
-cursor = conn.cursor()
-
 def criar_tabelas():
+    conn = sqlite3.connect('data.db')
+    cursor = conn.cursor()
     cursor.execute("""
     create table desafios(
         id integer NOT NULL AUTO INCREMENT PRIMARY KEY,
@@ -20,15 +19,33 @@ def criar_tabelas():
         mensagem text NOT NULL
     )
     """)
+    conn.close()
 
 def novo_desafio(lista):
+    conn = sqlite3.connect('data.db')
+    cursor = conn.cursor()
     cursor.execute("""
     insert into desafios(nome_desafio, resposta, link_imagem)
     values (?,?,?)
     """, lista)
+    conn.close()
+
 
 def novo_finalista(lista):
+    conn = sqlite3.connect('data.db')
+    cursor = conn.cursor()
     cursor.execute("""
     insert into finalistas(nick, data, mensagem)
     values(?,?,?)
     """,lista)
+    conn.close()
+
+
+def consultar_finalistas():
+    conn = sqlite3.connect('data.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+    SELECT * FROM finalistas;
+    """)
+    return cursor.fetchall()
+    conn.close()
