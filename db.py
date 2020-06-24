58057450c113d82d4ba240bbea6ad2105f6baa48
Igor-Felipy <igor.felipy123@gmail.com>
2020-06-24 13:23:43 -0400

    inserção de novos dados no banco feito

--- db.py
+++ db.py
@@ -1,48 +1,34 @@
 import sqlite3
 
-conn = sqlite3.connect('form_com_db/clientes.db')
-
+conn = sqlite3.connect('data.db')
 cursor = conn.cursor()
 
-def criar_banco():
+def criar_tabelas():
     cursor.execute("""
-    create table clientes (
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL,
-        idade INTEGER,
-        cpf VARCHAR(11) NOT NULL,
-        email TEXT NOT NULL,
-        fone TEXT,
-        cidade TEXT,
-        uf VARCHAR(2) NOT NULL,
-        criado_em DATE NOT NULL
-    );
-    """)
-
-    print('Tabela ciada com sucesso.')
-    conn.close()
-
-def registrar_teste():
-    cursor.execute("""
-    insert into clientes (nome,idade,cpf,email,fone,cidade,uf,criado_em)
-    values ('Regis','35','00000000000','regis@email.com','11-987654321','São Paulo','SP','2014-06-08')
+    create table desafios(
+        id integer NOT NULL AUTO INCREMENT PRIMARY KEY,
+        link_imagem text NOT NULL,
+        nome_desafio text NOT NULL,
+        resposta text NOT NULL, 
+    )
     """)
     cursor.execute("""
-    insert into clientes (nome,idade,cpf,email,fone,cidade,uf,criado_em)
-    values ('Aloisio','87','11111111111','aloisio@email.com','98765-4322','Porto Alegre','RS','2014-06-09')
+    create table finalistas(
+        id integer NOT NULL AUTO INCREMENT PRIMARY KEY,
+        nick text NOT NULL,
+        data date NOT NULL,
+        mensagem text NOT NULL
+    )
     """)
+
+def novo_desafio(lista):
     cursor.execute("""
-    insert into clientes (nome,idade,cpf,email,fone,cidade,uf,criado_em)
-    values ('Bruna','21','22222222222','bruna@email.com','21-98765-4323','Rio de Janeiro','RJ','2014-06-09')
-    """)
+    insert into desafios(nome_desafio, resposta, link_imagem)
+    values (?,?,?)
+    """, lista)
 
+def novo_finalista(lista):
     cursor.execute("""
-    insert into clientes (nome,idade,cpf,email,fone,cidade,uf,criado_em)
-    values ('Matheus','19','33333333333','matheus@email.com','11-98765-4324','Campinas','SP','2014-06-08')
-    """)
-
-    conn.commit()
-    print('Dados Inseridos com sucesso')
-    conn.close()
-
-
+    insert into finalistas(nick, data, mensagem)
+    values(?,?,?)
+    """,lista)
